ba09598087ddf074e498a2e19e796a34582df0e8
SashaD1337 <blizkota@gmail.com>
2025-02-01 04:51:47 +0300

    sdf

--- src/ShiftClass.java
+++ src/ShiftClass.java
@@ -6,54 +6,74 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ShiftClass {
     private static Path filePath;
+    private static String preffix = "";
+    private static boolean deleteFlag = true;
+    private static boolean shortStat = false;
+    private static AtomicInteger integerCount = new AtomicInteger(0);
+    private static AtomicInteger doubleCount = new AtomicInteger(0);
+    private static AtomicInteger stringCount = new AtomicInteger(0);
+    private static String integerFileName;
+
     public static void main(String[] args) {
-        startProg(new String[]{"-o","C:\\Users\\dviri\\IdeaProjects\\main\\ShiftTest\\src", "in1.txt", "in2.txt"});
+        startProg(new String[]{"-p", "preffix", "-s","in1.txt", "in2.txt"});
     }
 
     private static void startProg(String[] args) {
-
-
         List<String> strings = new ArrayList<>();
         for (int i = 0; i < args.length; i++) {
             if (args[i].endsWith(".txt")) {
                 strings.add(args[i]);
             } else {
                 switch (args[i]) {
-                    case "-s" -> System.out.println("параметры запуска S");
-                    case "-p" -> System.out.println("параметры запуска P");
-                    case "-o" -> filePath = setNewPath(args[i + 1]);
+                    case "-s" -> shortStat = true;
+                    case "-p" ->  preffix = setPreffix(args[i + 1]);
+                    case "-o" ->  filePath = setNewPath(args[i + 1]);
                     case "-f" -> System.out.println("параметры запуска F");
-                    case "-a" -> System.out.println("параметры запуска A");
+                    case "-a" -> deleteFlag = false;
                 }
             }
+         }
+        if (filePath == null) {
+            filePath = Paths.get(System.getProperty("user.dir"));
         }
+        deleteFiles(deleteFlag);
         strings.parallelStream().forEach(x -> {
             try {
                 readStrings(x);
             } catch (IOException e) {
                 System.out.println("Файл не найден");
-
             }
         });
+        if(shortStat){
+            printShortStat();
+        }
+    }
+
+
+
+    private static String setPreffix(String arg) {
+
+        return arg;
     }
 
     private static Path setNewPath(String path) {
+
         return Paths.get(path);
     }
 
 
+
     private static void readStrings(String arg) throws IOException {
-        Path filePath = Paths.get(arg);
+        Path path = Paths.get(arg);
         Pattern integerPattern = Pattern.compile("^-?\\d+$");
         Pattern doublePattern = Pattern.compile("^-?\\d+\\.\\d+$|^-?\\d+\\.\\d+E[+-]?\\d+$");
-        try (BufferedReader reader = Files.newBufferedReader(filePath)) {
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
             String line;
 
             while ((line = reader.readLine()) != null) {
@@ -64,11 +84,9 @@ public class ShiftClass {
                     toIntegerFile(line);
                     System.out.println("Это тип инт - " + line);
                 } else if (doubleMatcher.matches()) {
-
                     toDoubleFile(line);
                     System.out.println("Это тип дабл - " + line);
                 } else {
-                    String stringValue = line;
                     toStringFile(line);
                     System.out.println("Это тип стринг - " + line);
                 }
@@ -76,33 +94,46 @@ public class ShiftClass {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 
-    private static void writeToFile(String fileName, String content) throws IOException {
-        if (filePath == null) {
-            filePath = Paths.get(fileName);
-        }
+    private synchronized static void writeToFile(String fileName, String content) throws IOException {
+
+        Path fullPath = filePath.resolve(fileName);
 
 
+            Files.write(fullPath, (content + System.lineSeparator()).getBytes(),
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.APPEND);
 
-        Files.write(filePath, (content + System.lineSeparator()).getBytes(),
-                StandardOpenOption.CREATE,
-                StandardOpenOption.APPEND);
     }
 
     private static void toStringFile(String s) throws IOException {
-        writeToFile("string.txt", s);
+        writeToFile(preffix + "string.txt", s);
+        stringCount.incrementAndGet();
     }
 
     private static void toDoubleFile(String d) throws IOException {
-        writeToFile("double.txt", d);
+        writeToFile(preffix +"double.txt", d);
+        doubleCount.incrementAndGet();
     }
 
     private static void toIntegerFile(String i) throws IOException {
-        writeToFile("integer.txt", i);
+        writeToFile(preffix +"integer.txt", i);
+        integerCount.incrementAndGet();
     }
-
-
+    private static void deleteFiles(Boolean flag) {
+        if(flag) {
+            try {
+                Files.deleteIfExists(filePath.resolve(preffix + "string.txt"));
+                Files.deleteIfExists(filePath.resolve(preffix + "double.txt"));
+                Files.deleteIfExists(filePath.resolve(preffix + "integer.txt"));
+            } catch (IOException e) {
+                System.out.println("Ошибка при удалении файлов: " + e.getMessage());
+            }
+        }
+    }
+    private static void printShortStat() {
+        System.out.printf("Было добавлено строк в файл integer.txt - %d, double.txt - %d, string.txt - %d%n",
+                integerCount.get(), doubleCount.get(), stringCount.get());
+    }
 }
